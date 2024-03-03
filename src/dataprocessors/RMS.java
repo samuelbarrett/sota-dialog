910b4a66532c7fd45f3bfb78b8ca0dbb99c52c21
Jonathan Placatka <jplacatka@live.ca>
2024-03-03 16:20:05 -0600

    add window to RMS processor

--- src/dataprocessors/RMS.java
+++ src/dataprocessors/RMS.java
@@ -4,19 +4,36 @@ import datatypes.Data;
 import datatypes.DoubleData;
 
 public class RMS extends DataProcessor {
+
+    private double[] buffer;
+    private int index = 0;
+
+    public RMS(int windowSize) {
+        this.buffer = new double[windowSize];
+    }
+    
     @Override
     protected Data process(Data input) {
-
         //TODO handle ClassCastException
         DoubleData doubleInput = (DoubleData)input; 
         double[] data = doubleInput.data;
 
+        double[] output = new double[data.length];
+
+        for(int i = 0; i < data.length; i++) {
+            buffer[index] = data[i];
+            output[i] = rms();
+            index = (index + 1) % buffer.length;
+        }
+
+        return new DoubleData(output);
+    }
+
+    private double rms() {
         double sum = 0;
-        for(double d : data) {
+        for(double d : this.buffer) {
             sum += d*d;
         }
-        double rms = Math.sqrt(sum / data.length);
-        
-        return new DoubleData(new double[]{rms});
+        return Math.sqrt(sum / this.buffer.length);
     }
 }
