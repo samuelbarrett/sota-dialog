391a135312fa4cc64f29614ffd64e53057a3c192
Jonathan Placatka <jplacatka@live.ca>
2024-03-04 11:19:57 -0600

    add csv writer

---
+++ src/dataprocessors/CSVWriter.java
@@ -0,0 +1,43 @@
+package dataprocessors;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import datatypes.Data;
+import datatypes.DoubleData;
+
+public class CSVWriter extends DataProcessor {
+
+    private String filepath;
+    private BufferedWriter writer;
+
+    public CSVWriter(String filepath) {
+        this.filepath = filepath;
+    }
+
+    private void initWriter() throws IOException  {
+        if(this.writer == null) {
+            this.writer = Files.newBufferedWriter(Paths.get(this.filepath));
+        }
+    }
+
+    @Override
+    protected Data process(Data input) {
+        DoubleData doubleInput = (DoubleData)input; 
+        double[] data = doubleInput.data;
+
+        try {
+            initWriter();
+            for(double d : data) {
+                writer.write(String.valueOf(d)+",");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+    
+}
